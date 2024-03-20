7b2d0840c45460b7ee8639ce38695e1558276281
ichiyouda <tegami2@protonmail.com>
2024-03-20 08:38:26 +0800

    Update commands.sql
    ex 3.12 - sql format

--- commands.sql
+++ commands.sql
@@ -1,1 +1,9 @@
-CREATE TABLE IF NOT EXISTS "blogs" ("id"  SERIAL , "author" TEXT, "url" TEXT NOT NULL, "title" TEXT NOT NULL, "likes" INTEGER DEFAULT 0, PRIMARY KEY ("id"));
+CREATE TABLE IF NOT EXISTS "blogs" 
+(
+    "id" SERIAL,
+    "author" TEXT,
+    "url" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "likes" INTEGER DEFAULT 0,
+    PRIMARY KEY ("id")
+);
